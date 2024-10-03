00820afa1c2b1024573948522e366ab4c5169af8
Lev0nid <gp1279fg@gmail.com>
2024-10-03 22:58:27 +0600

    Added Everything

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import process
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Simple Decoder by Lev0nid")
+st.write("Simple decoding app by Lev0nid. Made for fun. Can decode lower case Russian letters.")
+
+form = st.form('input_text_form')
+text = form.text_area('Input text to get your text decoded')
+
+submitted = form.form_submit_button('Submit')
+
+decoder = process.Decoder()
+
+output_text = 'Input is empty'
+
+if submitted:
+    if text != '':
+        output_text = decoder.decode(text)
+    st.write(output_text)
